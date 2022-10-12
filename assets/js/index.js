2aa356d666e128b819bded8fd6af442de3af5b1d
juss-10 <100239793+juss-10@users.noreply.github.com>
2022-10-12 02:19:14 +0200

    Show books added to library

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,7 @@ const saveBookForm = document.querySelector('[data-library-form="save-book"]');
 const openBookFormButton = document.querySelector('[data-library-button="open-book-form"]');
 const submitBookFormButton = document.querySelector('[data-library-button="submit-book-form"]');
 const cancelBookFormButton = document.querySelector('[data-library-button="cancel-book-form"]');
+const currentBookListContainer = document.querySelector('[data-library-container="current-book"]');
 
 // Form inputs
 
@@ -12,6 +13,12 @@ const authorInput = document.querySelector('[data-library-input="author"]');
 const pageCountInput = document.querySelector('[data-library-input="page-count"]');
 const pagesReadInput = document.querySelector('[data-library-input="pages-read"]');
 const currentBookInput = document.querySelector('[data-library-input="current-book"]');
+
+// Book list containers
+
+const currentBook = document.querySelector('[data-library-list="current"]');
+const readingList = document.querySelector('[data-library-list="reading"]');
+const finishedReadingList = document.querySelector('[data-library-list="finished"]');
 
 // Event listeners
 
@@ -34,7 +41,11 @@ function closeModal(modal) {
 function saveBookHandler(event) {
     event.preventDefault()
     saveBookToLibrary()
+    showLibraryBook()
+    closeModal(saveBookModal)
 }
+
+// Create and store books
 
 function Book(title, author, pageCount, pagesRead, currentBook) {
     this.title = title;
@@ -62,4 +73,60 @@ function getPageCount() {
 
 function getPagesRead() {
     return Number(pagesReadInput.value)
+}
+
+// Show stored books
+
+function showLibraryBook() {
+    const book = myLibrary[myLibrary.length - 1];
+    const bookHtml = getBookHtml(book)
+    setBookHtml(book, bookHtml)
+}
+
+function getBookHtml() {
+    const book = myLibrary[myLibrary.length - 1];
+
+    return `
+        <div class="card">
+            <div class="card-image">
+                <span class="material-symbols-outlined card-image-icon">menu_book</span>
+            </div>
+            <div class="card-content">
+                <div class="card-top row">
+                    <div class="card-info">
+                        <h3 class="book-title">${book.title}</h3>
+                        <p class="book-author">${book.author}</p>
+                    </div>
+                    <div class="card-controls" data-library-book-id="${book.id}">
+                        <span class="material-symbols-outlined card-control" data-library-button="edit-book">edit</span>
+                        <span class="material-symbols-outlined card-control" data-library-button="delete-book">delete</span>
+                    </div>
+                </div>
+                <div class="card-bottom">
+                    <label class="meter-label">
+                        <span>${book.percentRead.toFixed(2)}% completed</span>
+                        <meter min="0" max="100" value="${book.percentRead}"></meter>
+                    </label>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+function setBookHtml(book, bookHtml) {
+    const html = DOMPurify.sanitize(bookHtml);
+
+    if (book.currentBook) {
+        showCurrentBook()
+        currentBook.innerHTML = html;
+        readingList.insertAdjacentHTML("beforeend", html)
+    } else if (!book.finishedReading) {
+        readingList.insertAdjacentHTML("beforeend", html)
+    } else {
+        finishedReadingList.insertAdjacentHTML("beforeend", html)
+    }
+}
+
+function showCurrentBook() {
+    currentBookListContainer.classList.add("block")
 }
